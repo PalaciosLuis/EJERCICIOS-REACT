80eed695a1cf26fea586a2d48568e709af5467dd
Luis Javier <924333523javier@gmail.com>
2022-11-13 18:05:02 -0500

    uso la funcionalidad en react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/pure/greeting';
+// import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/GreetingF';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/*componente propio*/}
-        <Greeting name="Luis palacios"></Greeting>
+        {/* <Greeting name="Luis palacios"></Greeting> */}
+        <GreetingF name={"lUIS 2 EN FUNCION"}></GreetingF>
       </header>
     </div>
   );
